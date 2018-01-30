9d04ead1a6fedbc4aaa37f36eab970eded67c676
Nicholas Dille <nicholas.dille@mailbox.org>
2018-01-30 13:33:20 +0100

    Strip module to bare minimum

--- HetznerCloud/HetznerCloud.psm1
+++ HetznerCloud/HetznerCloud.psm1
@@ -1,41 +1,10 @@
-$script:ModuleRoot = $PSScriptRoot
-$script:PSModuleVersion = "0.1"
-
+# Define internal data structure
 $script:HetznerCloud = @{
     Server = 'https://api.hetzner.cloud/v1'
     Token = $null
 }
 
-function Import-ModuleFile
-{
-	[CmdletBinding()]
-	Param (
-		[string]
-		$Path
-	)
-	
-	if ($doDotSource) { . $Path }
-	else { $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($Path))), $null, $null) }
+# Import functions
+Get-ChildItem -Path "$PSScriptRoot\Functions" -Filter '*.ps1' -Recurse | ForEach-Object {
+    . "$($_.FullName)"
 }
-
-# Detect whether at some level dotsourcing was enforced
-$script:doDotSource = $true
-if ($HetznerCloud_skipdotsourcemodule) { $script:doDotSource = $false }
-
-# Execute Preimport actions
-. Import-ModuleFile -Path "$ModuleRoot\internal\scripts\preimport.ps1"
-
-# Import all internal functions
-foreach ($function in (Get-ChildItem "$ModuleRoot\internal\functions\*.ps1" -ErrorAction Ignore))
-{
-	. Import-ModuleFile -Path $function.FullName
-}
-
-# Import all public functions
-foreach ($function in (Get-ChildItem "$ModuleRoot\functions\*.ps1" -ErrorAction Ignore))
-{
-	. Import-ModuleFile -Path $function.FullName
-}
-
-# Execute Postimport actions
-. Import-ModuleFile -Path "$ModuleRoot\internal\scripts\postimport.ps1"
