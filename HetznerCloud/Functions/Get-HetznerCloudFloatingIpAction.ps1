0c25c730583aaad5489305deb3f3dc718b7db254
Nicholas Dille <nicholas.dille@mailbox.org>
2018-02-02 20:22:07 +0100

    Implemented viewing floating IP actions

--- HetznerCloud/Functions/Get-HetznerCloudFloatingIpAction.ps1
+++ HetznerCloud/Functions/Get-HetznerCloudFloatingIpAction.ps1
@@ -1,1 +1,15 @@
-function Get-HetznerCloudFloatingIpAction {}
+function Get-HetznerCloudFloatingIpAction {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [int[]]
+        $Id
+    )
+
+    process {
+        $Id | ForEach-Object {
+            Invoke-HetznerCloudApi -Api 'floating_ips' -Id $_ -CustomAction 'actions' | Select-Object -ExpandProperty actions
+        }
+    }
+}
