845aefa740ab34f2eb2eadb177bccbfccf337ce9
Nicholas Dille <nicholas.dille@mailbox.org>
2018-03-18 14:05:05 +0100

    Added custom objects for metrics

--- HetznerCloud/Functions/Get-HetznerCloudServerMetric.ps1
+++ HetznerCloud/Functions/Get-HetznerCloudServerMetric.ps1
@@ -42,5 +42,93 @@ function Get-HetznerCloudServerMetric {
     }
 
     $TypeList = ($Type -join ',').ToLower()
-    Invoke-HetznerCloudApi -Api 'servers' -Id $Id -CustomAction "metrics?type=$($TypeList)&start=$($Start)&end=$($End)&step=$($Step)" | Select-Object -ExpandProperty metrics
+    $Data = Invoke-HetznerCloudApi -Api 'servers' -Id $Id -CustomAction "metrics?type=$($TypeList)&start=$($Start)&end=$($End)&step=$($Step)" | Select-Object -ExpandProperty metrics
+
+    $Data | ForEach-Object {
+        $Result = [pscustomobject]@{
+            Start = $_.start
+            End = $_.end
+            Steps = $_.step
+        }
+
+        if ($Type -contains 'CPU') {
+            $Values = $_.time_series.cpu.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Cpu -Value $Values
+        }
+
+        if ($Type -contains 'Disk') {
+            $Values = $_.time_series.'disk.0.iops.read'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Disk0IopsRead -Value $Values
+
+            $Values = $_.time_series.'disk.0.iops.write'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Disk0IopsWrite -Value $Values
+
+            $Values = $_.time_series.'disk.0.bandwidth.read'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Disk0BandwidthRead -Value $Values
+
+            $Values = $_.time_series.'disk.0.bandwidth.write'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Disk0BandwidthWrite -Value $Values
+        }
+
+        if ($Type -contains 'Network') {
+            $Values = $_.time_series.'network.0.pps.in'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Network0PpsIn -Value $Values
+
+            $Values = $_.time_series.'network.0.pps.out'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Network0PpsOut -Value $Values
+
+            $Values = $_.time_series.'network.0.bandwidth.in'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Network0BandwidthIn -Value $Values
+
+            $Values = $_.time_series.'network.0.bandwidth.out'.values | ForEach-Object {
+                [pscustomobject]@{
+                    Timestamp = $_[0]
+                    Value = $_[1]
+                }
+            }
+            Add-Member -InputObject $Result -MemberType NoteProperty -Name Network0BandwidthOut -Value $Values
+        }
+
+        $Result
+    }
 }
